1f6fb1438c26616565602e9c49d18b00c89bb222
VenshiGG <franekpru2008@gmail.com>
2025-12-05 14:14:33 +0100

    projekt

--- app/src/main/java/com/example/testoflagach/MainActivity.java
+++ app/src/main/java/com/example/testoflagach/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.testoflagach;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,7 +12,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    int licznik;
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,9 +24,19 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        textView=findViewById(R.id.textView);;
     }
 
     public void klik(View view) {
         view.setVisibility(view.INVISIBLE);
+        licznik++;
+        if(licznik==4){
+            //Toast.makeText(this, "Sigma wygrałeś", Toast.LENGTH_SHORT).show();
+            textView.setText("Sigma wygrałeś");
+        }
+    }
+
+    public void ok(View view) {
+        Toast.makeText(this, "Ten kolor jest w fladze Polski", Toast.LENGTH_SHORT).show();
     }
 }
